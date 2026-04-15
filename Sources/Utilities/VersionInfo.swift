310b9851ed477033dd6cdb624e254ad8680d2e89
Amirsalar Sojoudi <amirsalaar@gmail.com>
2026-04-15 14:41:49 -0700

    Update build metadata

--- Sources/Utilities/VersionInfo.swift
+++ Sources/Utilities/VersionInfo.swift
@@ -2,8 +2,8 @@ import Foundation
 
 struct VersionInfo {
     static let version = "2.1.0"
-    static let gitHash = "d5535a8deec70d81bf4ead64fa066333b90f8489"
-    static let buildDate = "2026-04-14"
+    static let gitHash = "22c17dfd0f6d833b76cb9c7ae2dc8cc25642e66b"
+    static let buildDate = "2026-04-15"
     
     static var displayVersion: String {
         if gitHash != "dev-build" && gitHash != "unknown" && !gitHash.isEmpty {
